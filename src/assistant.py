8b1eb1c5549e0a20e0cd7d22ab6a4b86daa48030
satya <satyamini@pop-os.localdomain>
2024-04-01 21:51:36 -0400

    added venv setup

---
+++ src/assistant.py
@@ -0,0 +1,9 @@
+"""
+Main file for Assistant Project.
+Assistant is initialized through here and then run through it's mental logical cycle. 
+"""
+import ollama 
+import pandas as pd
+
+class Assistant():
+    def __init__(self):
